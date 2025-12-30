42aeb97c233b3f7658fe20a555fdbf0e13c5ae98
Richard Fairthorne <richard.fairthorne@gmail.com>
2025-12-30 12:28:43 -0500

    Update pairqr to 0.1.13

--- Formula/pairqr.rb
+++ Formula/pairqr.rb
@@ -1,18 +1,18 @@
 class Pairqr < Formula
   desc "Pair Android devices for wireless ADB debugging by scanning a QR code"
   homepage "https://github.com/richard-fairthorne/pairqr"
-  version "0.1.12"
+  version "0.1.13"
   license "GPL-3.0"
 
   on_macos do
-    url "https://github.com/richard-fairthorne/pairqr/releases/download/v0.1.12/pairqr-macos-universal.tar.gz"
-    sha256 "f809c82d0d023c20796c867106abb976b80da88fc7c81a27d2dd4d59808b56c2"
+    url "https://github.com/richard-fairthorne/pairqr/releases/download/v0.1.13/pairqr-macos-universal.tar.gz"
+    sha256 "587c57a071b4cc8e8ca21316afefc74c572b134f922bb6241aae3ce301481811"
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/richard-fairthorne/pairqr/releases/download/v0.1.12/pairqr-linux-x86_64.tar.gz"
-      sha256 "1abca8261a2239d09003315c38f3eaf2a52801dd9f5cf1984ef808433c3f1540"
+      url "https://github.com/richard-fairthorne/pairqr/releases/download/v0.1.13/pairqr-linux-x86_64.tar.gz"
+      sha256 "5577f0572da727507ed1c7422f6e6c6149290994a13f7324208ff3666b6b58b0"
     end
   end
 
